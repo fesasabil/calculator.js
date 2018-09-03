91344476fdb71598d88a57ea4e8ffd84d767f712
fesasabil <kijee212@gmail.com>
2018-09-03 22:36:21 +0700

    calculator.js

---
+++ Rumus.js
@@ -0,0 +1,40 @@
+//rumuz kubus
+var kubus = 9;
+var kubus1 = 8;
+
+console.log(kubus * kubus * kubus); //jumlah = 729
+
+
+//rumuz persegi
+var persegi = 6;
+var persegi1 = 5;
+
+console.log(persegi1 * persegi1 * persegi1); //jumlah = 125
+
+
+//rumuz luas segitiga
+var segitiga = 7;
+var segitiga1 = 4;
+
+console.log(0.5 * segitiga * segitiga1 ); //jumlah = 14
+
+
+//rumuz tabung
+var phi = (22/7);
+var radius = 3;
+var tinggi = 5;
+
+console.log(phi * radius * radius * tinggi); //jumlah = 141.42857142857142
+
+
+//rumuz  luas jajar genjang
+var a = 7;
+var b = 4;
+
+console.log(a * b); //jumlah = 28
+
+
+
+
+
+
