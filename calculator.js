00caafa3ee95dfda48b4289b00f13425ca855258
fesasabil <kijee212@gmail.com>
2018-09-03 16:50:03 +0700

    calculator.js

---
+++ calculator.js
@@ -0,0 +1,33 @@
+//pertambahan
+var angka = 10;
+var angka1 = 20;
+
+console.log(angka + angka1); //nilai yang diinginkan berapa = 30
+
+//pengurangan
+var angka2 = 90;
+var angka3 = 93;
+
+console.log(angka2 - angka3);  //nilai yang diinginkan berapa = -3 
+
+
+//perkalian
+var angka4 = 350;
+var angka5 = 7;
+
+console.log(angka4 * angka5);  //nilai yang diinginkan berapa = 2450
+
+
+//pembagian
+var angka6 = 30;
+var angka7 = 10;
+
+console.log(angka6 / angka7);  //nilai yang diinginkan berapa = 3
+
+
+//modulus
+var angka8 = 2.400;
+var angka9 = 30;
+
+console.log(angka8 % angka9);  //nilai yang diinginkan berapa = 2.4
+
